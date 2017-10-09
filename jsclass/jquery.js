493468fecc2c04470cb0f2b6d1cd4adc16aae273
Bella-Wu <Bella-WU@users.noreply.github.com>
2017-10-09 01:27:57 -0400

    delete

--- jsclass/jquery.js
+++
@@ -1,9 +0,0 @@
-$('nav span').hover(function(){
-  console.log($(this).css('color'))
-  if($(this).css('color') == 'rgb(0, 0, 255)') {
-    $(this).css('color', 'pink')
-  } else {
-    $(this).css('color', 'blue')
-  }
-  /*console.log($(this).text()*/
-})
