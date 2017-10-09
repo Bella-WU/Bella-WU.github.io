b1eba69de21646b6e4d44f6b4184961aa0b21f2f
Bella-Wu <Bella-WU@users.noreply.github.com>
2017-10-09 01:23:21 -0400

    update

--- jsclass/jquery.js
+++ jsclass/jquery.js
@@ -1,3 +1,9 @@
-$('h1').hover() {
-	$('h1').css('color', 'black');
-}
+$('nav span').hover(function(){
+  console.log($(this).css('color'))
+  if($(this).css('color') == 'rgb(0, 0, 255)') {
+    $(this).css('color', 'pink')
+  } else {
+    $(this).css('color', 'blue')
+  }
+  /*console.log($(this).text()*/
+})
