b48c8d7e06bdaeec6930caaa78d6a8c68640597e
Bella-Wu <Bella-WU@users.noreply.github.com>
2017-10-24 13:22:38 -0400

    update

---
+++ interactive_website/sketch.js
@@ -0,0 +1,28 @@
+var beat;
+var airhorn;
+
+function preload(){
+  beat = loadSound('../assets/be.mp3');
+  airhorn = loadSound('../assets/airhorn.mp3');
+}
+
+function setup() {
+  createCanvas(720, 200);
+  background(255,0,0);
+}
+
+function mousePressed() {
+  if ( beat.isPlaying() ) { // .isPlaying() returns a boolean
+    beat.stop();
+    background(255,0,0);
+  } else {
+    beat.play();
+    background(0,255,0);
+  }
+}
+
+function keyTyped() {
+	if(key == " ") {
+		airhorn.play();
+	}
+}
